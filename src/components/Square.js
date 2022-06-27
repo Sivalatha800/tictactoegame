470678c2107ccf194f57ca71fd322f566fffb435
Sivalatha <rachamallusivaltha@gmail.com>
2022-06-27 22:06:28 +0530

    tic tac toe game

---
+++ src/components/Square.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Square = ({ value, onClick, isWinningSquares }) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={` square ${isWinningSquares ? "winning" : ""} ${
+        value === "X" ? "text-green" : "text-orange"
+      }`}
+      style={{ fontWeight: isWinningSquares ? "bold" : "normal" }}
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Square;
